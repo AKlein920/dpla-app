e4c621b5f430f1177d69363edb0514a6c240e35a
Abi Auslander <abi920@gmail.com>
2017-01-07 10:03:17 -0500

    First API call made

--- public/app.js
+++ public/app.js
@@ -1,5 +1,32 @@
 var app = angular.module('dpla-app', []);
 
-app.controller('BaseController as base', ['$http', function($http) {
-  
-}]);
+app.controller('BaseController', ['$http', function($http) {
+
+  var controller = this;
+
+  // need a function for randomly generated keyword to begin search
+
+  // function to get data from DPLA api:
+  this.getData = function(searchQuery) {
+
+    var ajaxUrl = 'http://api.dp.la/v2/items?q=' + searchQuery + '&api_key=7c4f10ae79cee82c4372d03dba940c74';
+
+    $http({
+      method: 'GET',
+      url: ajaxUrl
+    }).then({
+      function(response) {
+        // success
+        console.log(response);
+        controller.test = response.docs.object;
+        console.log(controller.test);
+      },
+      function(response) {
+        // failure
+        console.log(response);
+      }
+    }
+  )
+}
+
+}]); // end BaseController
