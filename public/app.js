918b7f6cb385d73ecbb216f4668bd3188663231f
CCAZ88 <camcardosi@mac.com>
2017-01-07 10:23:00 -0700

    successfully pulling the image from a query and populating it in html, but styling is off

--- public/app.js
+++ public/app.js
@@ -9,6 +9,7 @@ app.controller('BaseController', ['$http', function($http) {
   // function to get data from DPLA api:
   this.getData = function(searchQuery) {
     console.log('getting '+searchQuery+' data');
+    var controller = this;
     var ajaxUrl = 'http://api.dp.la/v2/items?q=' + searchQuery + '&sourceResource.type=image&page_size=25&api_key=7c4f10ae79cee82c4372d03dba940c74';
 
     $http({
@@ -17,9 +18,10 @@ app.controller('BaseController', ['$http', function($http) {
     }).then(
       function(response) {
         // success
-        console.log('getting response');
-        controller.test = response.docs.object;
-        console.log(controller.test);
+        console.log(response.data.docs[0].object);
+        controller.image = response.data.docs[0].object;
+        console.log(controller.image);
+        // console.log(controller.test);
       },
       function(response) {
         // failure
