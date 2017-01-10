ceb731733574ab8d321a846d5de1aaa06da11484
CCAZ88 <camcardosi@mac.com>
2017-01-10 16:14:43 -0700

    added body parser and set up middleware

--- server.js
+++ server.js
@@ -2,9 +2,21 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var app = express();
+var bodyParser = require('body-parser');
 
 //Middleware
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
+// var favoritesController = require('./controllers/FavoritesController.js');
+// app.use('/favorites', favoritesController);
+//
+// var sessionsController = require('./controllers/SessionsController.js');
+// app.use('/sessions', sessionsController);
+//
+// var usersController = require('./controllers/UsersController.js');
+// app.use('/users', usersController);
+
 mongoose.connect('mongodb://localhost:27017/dpla-app');
 mongoose.connection.once('open', function() {
   console.log('Connected to mongoD');
