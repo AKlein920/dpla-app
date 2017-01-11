fe77a514ff43bcd89f546779db746d7025ef795a
Abi Auslander <abi920@gmail.com>
2017-01-11 10:23:47 -0500

    deploying app to Heroku

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-//Require
+//Require dependencies
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+//Port
+var port = process.env.PORT || 3000;
+var mongoDBURI = process.env.MONGODB_URI ||
+'mongodb://localhost:27017/dpla-app';
+
 //Middleware
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
+//Require controller & controller middleware
 var favoritesController = require('./controllers/FavoritesController.js');
 app.use('/favorites', favoritesController);
 
-// var sessionsController = require('./controllers/SessionsController.js');
-// app.use('/sessions', sessionsController);
-//
-// var usersController = require('./controllers/UsersController.js');
-// app.use('/users', usersController);
-
-mongoose.connect('mongodb://localhost:27017/dpla-app');
-mongoose.connection.once('open', function() {
+//Database
+mongoose.connect(mongoDBURI);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
   console.log('Connected to mongoD');
 });
 
-app.listen(3000, function() {
-  console.log('Dpla app is listening');
+app.listen(port, function() {
+  console.log('Dpla app is running on port: ' + port);
 });
